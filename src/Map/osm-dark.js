c61a6357c39d374c0c0ae0f0b91afd9a46be8697
rizka.novia <rizka.novia@elhan.len.co.id>
2022-02-14 21:21:08 +0700

    Updates

--- src/Map/osm-dark.js
+++
@@ -1,8 +0,0 @@
-export default {
-    maptiler: { 
-        url:
-            "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-	    attribution: 
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-    },
-};
